c043eabc5555167e2f2d0d75c5654be74cbe5538
RenataCortes <cortesrenata1708@gmail.com>
2026-06-19 18:27:50 -0500

    feat: configurar alembic para migraciones y agregar paginacion de usuarios

---
+++ app/schemas/pagination.py
@@ -0,0 +1,16 @@
+from typing import Generic, TypeVar, List
+from pydantic import BaseModel
+
+# T es un comodín. Puede ser UserResponse, StudyRoomResponse, etc.
+T = TypeVar('T')
+
+class PaginationMeta(BaseModel):
+    total_records: int
+    current_page: int
+    total_pages: int
+    has_next: bool
+    has_previous: bool
+
+class PaginatedResponse(BaseModel, Generic[T]):
+    data: List[T]
+    meta: PaginationMeta
